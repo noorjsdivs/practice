e760214c475a8d2221ce015e3af0ee35bea078d1
habib_bin_jafor <habibjafor71@gmail.com>
2022-04-25 23:10:16 +0600

    added

---
+++ src/components/Habib.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Habib = () => {
+  return (
+    <>
+    <h1>Hi, i am Habib</h1>
+    <h2>Practice in github</h2>
+    <button>Send</button>
+    </>
+  )
+}
+
+export default Habib
