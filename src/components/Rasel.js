7fdf66832c209d2698b92b7b70d2c76da5ccf50a
noorjsdivs <104062645+noorjsdivs@users.noreply.github.com>
2022-04-25 22:54:16 +0400

    Rasel (#6)
    * This is our practice file
    
    * registration file added
    
    Co-authored-by: Rasel-Khalipha <mraselkhalifa@gmail.com>

--- src/components/Rasel.js
+++ src/components/Rasel.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "./components/Rasel.css";
+import { Form, ButtonToolbar, Button } from "rsuite";
 
 const Rasel = () => {
 	return (
 		<>
-			<h1>Hello</h1>
+			<div className="container">
+				{/*----------- form header start ------------- */}
+
+				<h1 className="w-full bg-lime-700">Registration</h1>
+
+				{/*----------- form header end ------------- */}
+
+				{/* --------------------- form area start ------------------ */}
+
+				<Form layout="horizontal">
+					<Form.Group controlId="name-6">
+						<Form.ControlLabel>Username</Form.ControlLabel>
+						<Form.Control name="name" />
+						<Form.HelpText>Please Enter your name</Form.HelpText>
+					</Form.Group>
+					<Form.Group controlId="email-6">
+						<Form.ControlLabel>Email Address</Form.ControlLabel>
+						<Form.Control name="email" type="email" />
+						<Form.HelpText>Please enter your email here</Form.HelpText>
+					</Form.Group>
+					<Form.Group controlId="password-6">
+						<Form.ControlLabel>Password</Form.ControlLabel>
+						<Form.Control name="password" type="password" autoComplete="off" />
+					</Form.Group>
+					<Form.Group controlId="password-6">
+						<Form.ControlLabel>Confirm Password</Form.ControlLabel>
+						<Form.Control name="password" type="password" autoComplete="off" />
+					</Form.Group>
+					<Form.Group>
+						<Button appearance="primary">Submit</Button>
+					</Form.Group>
+				</Form>
+
+				{/* --------------------- form area end ------------------ */}
+			</div>
 		</>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{js,jsx,ts,tsx}", ".index.js", "App.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/line-clamp")],
+	mode: "jit",
+	content: ["./src/**/*.{js,jsx,ts,tsx}", ".index.js", "App.js"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/line-clamp")],
 };
