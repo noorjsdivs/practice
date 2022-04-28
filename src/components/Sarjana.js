f1df2807bb1337d845734c5a3951d690e8315c00
sarjanahaque <sarjanahaquearpi@gmail.com>
2022-04-28 12:17:43 +0600

    1st commit

---
+++ src/components/Sarjana.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const sarjana = () => {
+  return (
+    <>
+    <h1>ami arpi..
+      akhon kisu banalo na age merge koren vai.
+    </h1>
+    </>
+  )
+}
+
+export default sarjana
