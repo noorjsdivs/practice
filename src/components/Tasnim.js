85081c27e0da61966dfcdb6f6999f03cfdc583d5
Dipinti <tarannoomdipinti@gmail.com>
2022-05-08 04:51:59 +0600

    add function

--- src/components/Tasnim.js
+++ src/components/Tasnim.js
@@ -1,10 +1,43 @@
-import React,{useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from './Header'
 import '../css/tasnim.css'
 
 const Tasnim = () => {
 
+  let [number, setNumber]  = useState("");
+  let [numbers, setNumbers] = useState([]);
+  let [operator, setOperator] = useState("");
+  let [operators, setOperators] = useState([]);
+  let [sum, setSum] = useState("");
+  let [ans, setAns] = useState(0);
   
+
+
+
+  let handleNumber = (e) => {
+    setNumber(e.target.value);
+    numbers.push(parseInt(e.target.value));
+  }
+  
+  let handleOperator = (e) => {
+    setOperator(e.target.value);
+    operators.push(e.target.value);
+  }
+  let handleAns = () => {
+        sum = numbers[0];
+        numbers.forEach((item,index)=>{
+            if(operators[index] == '+'){
+                
+                sum = sum + numbers[index+1];
+                
+            }    
+        }
+        )
+        setAns(sum);
+    }
+
+
+
 
   return (
       <>
@@ -12,33 +45,33 @@ const Tasnim = () => {
       <h2> Calculator Program in ReactJS </h2>
         <form className='calculator'>
         
-            <input type = "button" value = "1" />
-            <input type = "button" value = "2" />
-            <input type = "button" value = "3" />
-            <input type = "button" value = "+" />
+            <input type = "button" value = "1" onClick={handleNumber}/>
+            <input type = "button" value = "2" onClick={handleNumber}/>
+            <input type = "button" value = "3" onClick={handleNumber}/>
+            <input type = "button" value = "+" onClick={handleOperator}/>
             <br/> 
             
-            <input type = "button" value = "4" />
-            <input type = "button" value = "5" />
-            <input type = "button" value = "6" />
-            <input type = "button" value = "-" />
+            <input type = "button" value = "4" onClick={handleNumber}/>
+            <input type = "button" value = "5" onClick={handleNumber}/>
+            <input type = "button" value = "6" onClick={handleNumber}/>
+            <input type = "button" value = "-" onClick={handleOperator}/>
             <br/> 
             
-            <input type = "button" value = "7" />
-            <input type = "button" value = "8" />
-            <input type = "button" value = "9" />
-            <input type = "button" value = "*" />
+            <input type = "button" value = "7" onClick={handleNumber}/>
+            <input type = "button" value = "8" onClick={handleNumber}/>
+            <input type = "button" value = "9" onClick={handleNumber}/>
+            <input type = "button" value = "*" onClick={handleOperator}/>
             <br/> 
             
             
-            <input type = "button" value = "/" />
-            <input type = "button" value = "0" />
+            <input type = "button" value = "/" onClick={handleOperator}/>
+            <input type = "button" value = "0" onClick={handleNumber}/>
             <input type = "button" value = "." /> 
-            <input type = "button" value = "=" />
+            <input type = "button" value = "=" onClick={handleAns}/>
             <br/> 
 
             <input type = "button" value = "C" className="clear"/>
-            <input type = "text" name = "ans" readOnly/> 
+            <input type = "text" value= {ans} readOnly/> 
             <br/> 
   
         </form>
